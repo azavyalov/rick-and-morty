d37c921b08fb1ccbce2169ff93433b437c5e9111
zavyalov_al <zavyalov_al@bifit.com>
2021-03-23 17:49:00 +0300

    WIP

--- app/src/main/java/com/azavyalov/rickandmorty/MainActivity.kt
+++ app/src/main/java/com/azavyalov/rickandmorty/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.azavyalov.rickandmorty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.azavyalov.rickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
