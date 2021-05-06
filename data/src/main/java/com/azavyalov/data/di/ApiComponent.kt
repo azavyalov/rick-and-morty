03f0cc2005012e0bf8f581c58295df5289d81377
zavyalov_al <zavyalov_al@bifit.com>
2021-05-06 16:35:53 +0300

    Add Singleton scope

--- data/src/main/java/com/azavyalov/data/di/ApiComponent.kt
+++ data/src/main/java/com/azavyalov/data/di/ApiComponent.kt
@@ -3,8 +3,10 @@ package com.azavyalov.data.di
 import com.azavyalov.data.repository.CharactersRepository
 import com.azavyalov.data.repository.EpisodesRepository
 import dagger.Component
+import javax.inject.Singleton
 
 @Component(modules = [ApiModule::class])
+@Singleton
 interface ApiComponent {
 
     fun inject(repository: CharactersRepository)
--- data/src/main/java/com/azavyalov/data/di/ApiModule.kt
+++ data/src/main/java/com/azavyalov/data/di/ApiModule.kt
@@ -9,11 +9,13 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 class ApiModule {
 
     @Provides
+    @Singleton
     fun provideCharacterApi(): CharactersApi {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
